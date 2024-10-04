0b36624c6cbdd47817212bd0cb55fba31d855bab
Aditya <aditya@Adityas-MacBook-Air.local>
2024-10-05 00:28:24 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Aditya',
+    author_email='pandey.aditya2304@mail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+
+)
